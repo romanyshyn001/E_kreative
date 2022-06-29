54b6ebcfb731100ed9b9bb6184c529fee80d7e01
unknown <roman1romanyshyn@gmail.com>
2022-06-29 10:31:57 +0300

    fixed issues

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
+
 import { configureStore } from "@reduxjs/toolkit";
 import posts from "./slices/article";
-=======
-import { configureStore } from '@reduxjs/toolkit'
-import createSagaMiddleware from '@redux-saga/core'
-
-import moviesList from './slices/films'
-import movieDetailsInfo from './slices/movieDetailsInfo'
-import characterInfo from './slices/characterInfo'
-import rootSaga from './slices/rootSaga'
->>>>>>> 3d7d3b84a8b261690f1209447a459f980bff15b3
 
 import { rootSaga } from "./saga/rootSaga";
 import createSagaMiddleware from '@redux-saga/core'
@@ -20,7 +11,6 @@ let sagaMiddleware = createSagaMiddleware()
 const middleware = [sagaMiddleware]
 
 export default configureStore({
-<<<<<<< HEAD
     reducer: {
         posts,
         comment: comments,
@@ -30,17 +20,5 @@ export default configureStore({
         return getDefaultMiddleware({ thunk: false }).prepend(sagaMiddleware)
     }
 })
-=======
-  reducer: {
-    movies: moviesList,
-    movieDetails: movieDetailsInfo,
-    personage: characterInfo
-  },
-  
-  middleware: getDefaultMiddleware => {
-    return getDefaultMiddleware({ thunk: false }).prepend(sagaMiddleware)
-}
-})
 
->>>>>>> 3d7d3b84a8b261690f1209447a459f980bff15b3
 sagaMiddleware.run(rootSaga)
